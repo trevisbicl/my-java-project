65fcc5c741f6bdd4b6a463b4f1e53dd3e77fa21f
king crimson <kc653965@gmail.com>
2024-09-27 09:21:16 +0600

    Litcode35.java accapted

---
+++ Beecrowd/src/Litcode35.java
@@ -0,0 +1,24 @@
+public class Litcode35 {
+    public int searchInsert(int[] nums, int target) {
+        int a = 0;
+        int b = nums.length-1;
+
+        while(a<=b)
+        {
+            int  mid = a + (b - a) / 2;
+            if(nums [mid]==target)
+            {
+                return mid;
+            }
+            if (nums[mid]<target)
+            {
+                a= mid+1;
+            }
+            if (nums[mid]>target)
+            {
+                b=mid-1;
+            }
+        }
+        return a;
+    }
+}
