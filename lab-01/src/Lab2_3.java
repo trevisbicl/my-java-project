d27683e88504adab62d313b6a987559f27e49d27
king crimson <kc653965@gmail.com>
2024-10-04 22:59:08 +0600

    Lab2_1,2_2,2_3.java accapted

---
+++ lab-01/src/Lab2_3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Lab2_3 {
+    public static void main(String[] args) {
+
+        double feet;
+        double meters;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the feet: ");
+        feet = input.nextDouble();
+        meters = feet * 0.305;
+        System.out.println(feet + " is " + meters + " meters");
+    }
+}
