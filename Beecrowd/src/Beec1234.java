3a3fea85cb28a8ec89514703d2fd5db84e4a2e4e
king crimson <kc653965@gmail.com>
2024-10-11 09:04:24 +0600

    Beec1193,1234.java accapted

--- Beecrowd/src/Beec1234.java
+++ Beecrowd/src/Beec1234.java
@@ -2,34 +2,32 @@ import java.util.Scanner;
 
 public class Beec1234 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        String str;
 
-        while (sc.hasNextLine()) {
-            String sentence = sc.nextLine();
-            String result = DacnceDead(sentence);
+        while (in.hasNextLine()) {
+            str = in.nextLine();
+            int j = 0;
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+
+                if (c != ' ') {
+                    j++;
+                    if (j % 2 == 1) {
+                        result.append(Character.toUpperCase(c));
+                    } else {
+                        result.append(Character.toLowerCase(c));
+                    }
+                } else {
+                    result.append(" ");
+                }
+            }
+
             System.out.println(result);
         }
-    }
 
-    public static String DacnceDead(String sentence) {
-        String result = "";
-        boolean isUpper = true;
-
-        for (int i = 0; i < sentence.length(); i++) {
-            char currentChar = sentence.charAt(i);
-
-            if (currentChar != ' ') {
-                if (isUpper) {
-                    result += Character.toUpperCase(currentChar);
-                } else {
-                    result += Character.toLowerCase(currentChar);
-                }
-                isUpper = !isUpper;
-            } else {
-                result += " ";
-            }
-        }
-
-        return result;
+        in.close();
     }
 }
